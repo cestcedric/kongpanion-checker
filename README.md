6949076656c43f4889f4288d0cc61184802991f1
Cedric Stark <cedric.stark@outlook.com>
2023-02-08 00:28:45 +0100

    Retrieve all necessary info and display current weekly Kongpanion

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 
 Simple website to check which Kongpanions you have, which you miss, and which one is up for grabs right now.
 
-## Notes
+## TODO
+
+- Clear button in username input
+- Some individual styling
+- Tabs or something
+- Add dark mode
+
+## Resources
 
 - All Kongpanions: https://docs.kongregate.com/reference/server-api-kongpanions
 - User Kongpanions: https://docs.kongregate.com/reference/server-api-user-kongpanions
-- HTTP requests: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
--- src/scripts/kongpanionChecker.js
+++ src/scripts/kongpanionChecker.js
@@ -1,3 +1,8 @@
+// Proxy from https://github.com/gnuns/allorigins
+const corsProxy = (url) => {
+  return `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`;
+};
+
 const kongpanionOverviewPath = 'https://www.kongregate.com/kongpanions';
 
 const allKongpanionsPath =
@@ -5,21 +10,81 @@ const allKongpanionsPath =
 const userKongPanionsPath = (user) =>
   `https://api.kongregate.com/api/kongpanions.json?username=${user}`;
 
-const addDestination = (dest) => {
-  const destinationList = document.getElementById('destinations');
-  const item = document.createElement('li');
-  item.appendChild(document.createTextNode(dest));
-  destinationList.appendChild(item);
+const getAllKpans = async () => {
+  return fetch(allKongpanionsPath)
+    .then((response) => {
+      if (response.ok) return response.json();
+      throw new Error(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  // initial loading of hardcoded list
-  destinations.forEach(addDestination);
+const getUserKpans = async (user) => {
+  return fetch(corsProxy(userKongPanionsPath(user)))
+    .then((response) => response.json())
+    .then((data) => JSON.parse(data.contents))
+    .then((data) => {
+      if (data.success) return data;
+      throw new Error(data.error_description);
+    })
+    .catch((error) => window.alert(`Oh no!\n${error}`));
+};
 
-  // update list and display new submission
-  document.querySelector('#submit').addEventListener('click', () => {
-    const input = document.querySelector('input');
-    addDestination(input.value);
-    input.value = '';
+const getWeeklyKpan = async () => {
+  return fetch(corsProxy(kongpanionOverviewPath))
+    .then((response) => response.json())
+    .then(traverseDomWeeklyKpan)
+    .catch((error) => console.log(error));
+};
+
+const traverseDomWeeklyKpan = (data) => {
+  const parser = new DOMParser();
+  const content = parser.parseFromString(data.contents, 'text/html');
+  const kpanContent = Array.from(
+    content.getElementsByClassName('cur_kpan regtext ')[0].childNodes
+  );
+
+  const kpanImg = kpanContent[1].src;
+  const kpanImgAlt = kpanContent[1].alt;
+  const kpanName = Array.from(
+    Array.from(kpanContent[3].childNodes)[1].childNodes
+  )[0].data;
+  const kpanDescription = kpanContent[5].innerText;
+
+  return { kpanImg, kpanImgAlt, kpanName, kpanDescription };
+};
+
+const addWeeklyKpanInfo = ({
+  kpanImg,
+  kpanImgAlt,
+  kpanName,
+  kpanDescription,
+}) => {
+  const imgDiv = document.getElementById('weeklyKpanImg');
+  const img = document.createElement('img');
+  img.src = kpanImg;
+  img.alt = kpanImgAlt;
+  imgDiv.appendChild(img);
+
+  const textDiv = document.getElementById('weeklyKpanText');
+  const nameElement = document.createElement('h4');
+  const descriptionElement = document.createElement('p');
+  nameElement.innerHTML = kpanName;
+  descriptionElement.innerHTML = kpanDescription;
+  textDiv.appendChild(nameElement);
+  textDiv.appendChild(descriptionElement);
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const weeklyKpan = await getWeeklyKpan();
+  addWeeklyKpanInfo(weeklyKpan);
+
+  document.querySelector('#submit').addEventListener('click', async () => {
+    const user = document.querySelector('input').value;
+
+    const allKpans = await getAllKpans();
+    const userKpans = await getUserKpans(user);
   });
 });
